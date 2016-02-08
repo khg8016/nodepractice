80a136f7c3d480f062114005b67ae96bb50b46d8
khg8016 <khg8016@nate.com>
2016-02-08 18:40:13 +0900

    1차

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var mongoose = require('mongoose');
+var app = express();
+mongoose.connect('mongodb://localhost:27017/prdb'); // 기본 설정에 따라 포트가 상이 할 수 있습니다.
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("mongo db connection OK.");
+});
+
+var memoShema =mongoose.Schema({
+  username : String,
+  memo : String
+});
+var memoModel = mongoose.model('memo1', memoShema);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', routes);
+app.post('/insert', function (req,res,err) {
+  console.log("post!!!");
+  var memo = new memoModel({username: req.body.username, memo: req.body.memo});
+  memo.save(function (err) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    res.end("success");
+  });
+});
+app.use('/check', function(req, res, next) {
+  memoModel.find({},function(err,docs){
+    console.log(docs);
+  })
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
